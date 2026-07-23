de58aefb505f388436a5f530cb0cce4df0f0555e
Chaitanya Kamble <chaitanyakamble2005@gmail.com>
2026-07-23 22:31:44 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/connectDb.js"
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT
+
+app.get("/", (req, res) => {
+    return res.json({message: "Server Started"})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+    connectDb()
+})
